4ddf9e6dd5c67ff3757953315a45dadb295afe29
jagdish75016-max <jagdish75016@gmail.com>
2026-03-23 10:35:13 +0530

    Create favcolor.php

---
+++ favcolor.php
@@ -0,0 +1,17 @@
+<?php
+$ fav color="red";
+switch(($fav color)
+{
+case"red":
+    echo"Your favourite color is red!";
+    break;
+case"blue:
+    echo"Your favourite color is blue!";
+    break;
+case green:
+    echo"Your favourite color is green!";
+    break;
+default:
+    echo"Your favourite color is neither red,blue,nor green!";
+}
+?>
